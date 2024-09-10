ad5f671ad55da88c0b07fffe3a578f4aa3f1aa58
CDSHotels - Daniele Totisco <d.totisco@cdshotels.it>
2024-09-10 19:21:43 +0200

    initial commit

---
+++ LightModel.cs
@@ -0,0 +1,11 @@
+public class LightModel
+{
+   //[JsonPropertyName("id")]
+   //public int Id { get; set; }
+
+   //[JsonPropertyName("name")]
+   //public string Name { get; set; }
+
+   //[JsonPropertyName("is_on")]
+   //public bool? IsOn { get; set; }
+}
